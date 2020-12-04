fb7dc1d31dd6f52de2ee2c3e442ac4799755c970
@jonatas.viana <56093381+jonatasvnascimento@users.noreply.github.com>
2020-12-04 16:50:11 -0300

    Jquery fix

--- src/main/App.jsx
+++ src/main/App.jsx
@@ -1,12 +1,10 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'font-awesome/css/font-awesome.min.css'
-import 'jquery'
 import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
 
 import Routes from './Routes'
 import Header from '../tamplates/Header'
-import Home from '../tamplates/Home'
 
 
 
