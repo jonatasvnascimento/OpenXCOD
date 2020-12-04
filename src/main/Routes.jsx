492401923486070fc2e83f8752c3984accd356ba
@jonatas.viana <56093381+jonatasvnascimento@users.noreply.github.com>
2020-12-04 09:35:24 -0300

    add usercrud

---
+++ src/main/Routes.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import {Switch, Route, Redirect} from 'react-dom'
+
+import Footer from '../tamplates/Footer'
+
+
+export default function exit(){
+    <Switch>
+        <Route exact path='/' component={Footer}/>
+    </Switch>
+}
