c891669716a4784b12616f3b95202be0de093c1e
@jonatas.viana <56093381+jonatasvnascimento@users.noreply.github.com>
2020-12-07 19:52:09 -0300

    Teste Card

--- src/tamplates/Tools.jsx
+++ src/tamplates/Tools.jsx
@@ -36,6 +36,39 @@ export default function Tools() {
                             </div>
                         </div>
                     </div>
+                    <div className="col-sm-3 mb-3">
+                        <div className="card text-white bg-dark mb-3">
+                            <div class="card-body">
+                                <h5 class="card-header text-center">Converter Texto</h5>
+                                <p class="card-text mb-1">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            </div>
+                            <div class="card-footer">
+                                <Link to="#" className="btn btn-secondary">Começar</Link>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-sm-3 mb-3">
+                        <div className="card text-white bg-dark mb-3">
+                            <div class="card-body">
+                                <h5 class="card-header text-center">Converter Texto</h5>
+                                <p class="card-text mb-1">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            </div>
+                            <div class="card-footer">
+                                <Link to="#" className="btn btn-secondary">Começar</Link>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-sm-3 mb-3">
+                        <div className="card text-white bg-dark mb-3">
+                            <div class="card-body">
+                                <h5 class="card-header text-center">Converter Texto</h5>
+                                <p class="card-text mb-1">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            </div>
+                            <div class="card-footer">
+                                <Link to="#" className="btn btn-secondary">Começar</Link>
+                            </div>
+                        </div>
+                    </div>
 
                    
 
