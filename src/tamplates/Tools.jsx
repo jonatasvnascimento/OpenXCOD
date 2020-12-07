3ccdc85d8ea0bc9cdb208fa32b90be2531dbb33d
@jonatas.viana <56093381+jonatasvnascimento@users.noreply.github.com>
2020-12-07 17:45:49 -0300

    Tools update

--- src/tamplates/Tools.jsx
+++ src/tamplates/Tools.jsx
@@ -5,34 +5,41 @@ import { Link } from 'react-router-dom'
 export default function Tools() {
     return (
         <div className="container">
-            <div className="row row-cols-0 row-cols-md-4 justify-content-center ">
 
-                {/* Refatorar Card em componente separado */}
+            <div className="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
+                <h1 className="display-4">Ferramentas</h1>
+                <p className="lead">Area destina a Ferramentas para auxiliar trabalhos da faculdade, calculos matematicos, entre outros...</p>
+            </div>
 
-                <div className="card-deck ">
-                    <div className="card text-white bg-dark mb-3 mr-4 ">
-                        <div className="card-body ">
-                            <div className="card-header d-flex justify-content-center ">Conversor de Texto</div>
-                            <p className="card-text ">Conversor de texto com muitas funcionabilidades</p>
-                        </div>
-                        <div className="card-footer">
-                            <Link to="/text-convert" className="btn btn-primary ">Começar</Link>
-                        </div>
-                    </div>
-                </div>
-                <div className="card-deck ">
-                    <div className="card text-white bg-dark mb-3 mr-4">
-                        <div className="card-body ">
-                            <div className="card-header d-flex justify-content-center ">Calculadora Cientifica</div>
-                            <p className="card-text ">Projeto calculadora cientifica</p>
-                        </div>
-                        <div className="card-footer">
-                            <Link to="#" className="btn btn-primary ">Começar</Link>
+            <div className="card-deck mb-3">
+                <div className="row">
+
+                    <div className="col-sm-3 mb-3">
+                        <div className="card text-white bg-dark mb-3">
+                            <div class="card-body">
+                                <h5 class="card-header text-center">Converter Texto</h5>
+                                <p class="card-text mb-1">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            </div>
+                            <div class="card-footer">
+                                <Link to="#" className="btn btn-secondary">Começar</Link>
+                            </div>
                         </div>
                     </div>
+                    <div className="col-sm-3 mb-3">
+                        <div className="card text-white bg-dark mb-3">
+                            <div class="card-body">
+                                <h5 class="card-header text-center">Converter Texto</h5>
+                                <p class="card-text mb-1">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            </div>
+                            <div class="card-footer">
+                                <Link to="#" className="btn btn-secondary">Começar</Link>
+                            </div>
+                        </div>
+                    </div>
+
+                   
+
                 </div>
-
-
             </div>
         </div>
     )
