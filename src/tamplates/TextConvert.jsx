be6c7855607862063d5c3f09ddbad474cfb5ecb3
@jonatas.viana <56093381+jonatasvnascimento@users.noreply.github.com>
2020-12-09 14:50:49 -0300

    Finish layout Codigo

--- src/tamplates/TextConvert.jsx
+++ src/tamplates/TextConvert.jsx
@@ -63,34 +63,52 @@ const TextConvert = (props) => {
                     <Row>
                         <Col>
                             <div className="container">
+                                <div className="form-group mt-3">
+                                    <label >Conversor de Base</label>
+                                    <select className="form-control">
+                                        <option>Binary to Decinal</option>
+                                        <option>Binary to Hexa</option>
+                                        <option>Binary to Octa</option>
+                                        <option>Decimal to Hexa</option>
+                                        <option>Decimal to Octa</option>
+                                        <option>Decimal to Binary</option>
+                                        <option>Hexa to Decimal</option>
+                                        <option>Hexa to Octa</option>
+                                        <option>Hexa to Binary</option>
+                                        <option>Octa to Decimal</option>
+                                        <option>Octa to Hexa</option>
+                                        <option>Octa to Binary</option>
+                                        <option>Texto to Binary</option>
+                                        <option>Texto to Hexa   </option>
+                                    </select>
+                                </div>
+
                                 <div className="row mt-3">
-                                    <div className="row">
-                                        <ul class="list-group-flush">
-                                            <li class="list-group-item"><Link to="#" className="btn btn-secondary">BINARIO</Link></li>
-                                            <li class="list-group-item"><Link to="#" className="btn btn-secondary">ASCII</Link></li>
-                                            <li class="list-group-item"><Link to="#" className="btn btn-secondary">HEX</Link></li>
-                                            <li class="list-group-item"><Link to="#" className="btn btn-secondary">MORSE</Link></li>
-                                        </ul>
-                                        <div className="col-sm-4">
-                                            <div className="card">
-                                                <div className="card-body">
-                                                    <h5 className="card-title">Codificador</h5>
-                                                    <div className="form-group">
-                                                        <textarea className="form-control" rows="12" cols="60"></textarea>
-                                                    </div>
-                                                    <Link to="#" className="btn btn-primary">Codificar</Link>
+
+                                    <ul className="list-group">
+                                        <button type="button" className="btn btn-secondary mb-2">Morse</button>
+                                        <button type="button" className="btn btn-secondary mb-2">ASCII</button>
+
+                                    </ul>
+                                    <div className="col-sm-4">
+                                        <div className="card">
+                                            <div className="card-body">
+                                                <h5 className="card-title">Codificador</h5>
+                                                <div className="form-group">
+                                                    <textarea className="form-control" rows="12" cols="60"></textarea>
                                                 </div>
+                                                <Link to="#" className="btn btn-primary">Codificar</Link>
                                             </div>
                                         </div>
-                                        <div className="col-sm-4">
-                                            <div className="card">
-                                                <div className="card-body">
-                                                    <h5 className="card-title">Decodificador</h5>
-                                                    <div className="form-group">
-                                                        <textarea className="form-control" rows="12" cols="60"></textarea>
-                                                    </div>
-                                                    <Link to="#" className="btn btn-primary">Decodificar</Link>
+                                    </div>
+                                    <div className="col-sm-4">
+                                        <div className="card">
+                                            <div className="card-body">
+                                                <h5 className="card-title">Decodificador</h5>
+                                                <div className="form-group">
+                                                    <textarea className="form-control" rows="12" cols="60"></textarea>
                                                 </div>
+                                                <Link to="#" className="btn btn-primary">Decodificar</Link>
                                             </div>
                                         </div>
                                     </div>
