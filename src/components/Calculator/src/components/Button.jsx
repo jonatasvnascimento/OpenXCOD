f6d8a7bf6782c784fe9e1a32480027b27799d67e
@jonatas.viana <56093381+jonatasvnascimento@users.noreply.github.com>
2020-12-13 15:39:05 -0300

    Add Calculator Project Github

--- src/components/Calculator/src/components/Button.jsx
+++ src/components/Calculator/src/components/Button.jsx
@@ -2,9 +2,20 @@ import React from 'react'
 import './Button.css'
 
 export default function Button(props){
-    return(
-        <div>
-            <button className="button">{props.label}</button>
-        </div>
+    let classes = 'button '
+    classes += props.operation ? 'operation' : ''
+    classes += props.double ? 'double' : ''
+    classes += props.triple ? 'triple' : ''
+    classes += props.quad ? 'quad' : ''
+    classes += props.clear ? 'clear' : ''
+    classes += props.equals ? 'equals' : ''
+    
+    return (
+        <button
+            onClick={e => props.click && props.click(props.label)}
+            className={classes}>
+
+            {props.label}    
+        </button>
     )
 }
