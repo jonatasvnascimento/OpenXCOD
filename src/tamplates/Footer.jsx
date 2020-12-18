ca46e8ce7aef7ce3204999449e6596f5440d70ce
@jonatas.viana <56093381+jonatasvnascimento@users.noreply.github.com>
2020-12-17 23:40:59 -0300

    Linux mi

--- src/tamplates/Footer.jsx
+++ src/tamplates/Footer.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import './css/Footer.css'
 import { Link } from 'react-router-dom'
 
-// Mexer no tamanho do footer
 
 export default function Footer(props) {
     return (
