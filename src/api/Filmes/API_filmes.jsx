b48bf4e3b6271cce2e8c5e897a7d445e35645e94
jonatasvnascimento <jonatasv.nascimento@gmail.com>
2021-05-28 14:10:37 -0300

    api key

--- src/api/Filmes/API_filmes.jsx
+++ src/api/Filmes/API_filmes.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const Api_filmes = axios.create({
-    baseURL: 'http://api.tvmaze.com/search/shows?q=start%20war'
+    baseURL: 'http://api.tvmaze.com/search/shows?q='
 })
 
 
