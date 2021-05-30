88cfe35e9d5a81d2c8201cb2d7efac2e0351d109
jonatasvnascimento <jonatasv.nascimento@gmail.com>
2021-05-30 17:36:56 -0300

    linktoo

--- src/components/LinkTools.jsx
+++ src/components/LinkTools.jsx
@@ -7,10 +7,10 @@ export default function LinkTools(props) {
             <div className="card text-white bg-dark mb-3">
                 <div className="card-header">{props.title}</div>
                 <div className="card-body">
-                    <h2 class=" mb-4 display-6 lh-1 fw-bold">Short title, long jacket</h2>
+                        <h2 class=" mb-4 display-7 lh-1 fw-bold">{props.title}</h2>
                     <ul class="d-flex list-unstyled mt-auto">
                         <li class="me-auto">
-                            <img src="https://github.com/twbs.png" alt="Bootstrap" width="32" height="32" class="rounded-circle border border-white" />
+                            {props.icon}
                         </li>
                         <li class="d-flex align-items-center me-3">
                             <svg class="bi me-2" width="1em" height="1em"></svg>
@@ -21,6 +21,7 @@ export default function LinkTools(props) {
                             <small>3d</small>
                         </li>
                     </ul>
+                        <Link to={props.linkgo} className="btn btn-secondary">Começar</Link>
                 </div>
             </div>
         </div>
