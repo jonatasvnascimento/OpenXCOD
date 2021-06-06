332089838f3fd7f893f7b0a3bc8ba446c89c63f0
jonatasvnascimento <jonatasv.nascimento@gmail.com>
2021-06-06 01:10:49 -0300

    add img

--- src/Entretenimento/SimponAllTemp/Simpson.jsx
+++ src/Entretenimento/SimponAllTemp/Simpson.jsx
@@ -58,7 +58,7 @@ export default function Simpson() {
                         title="Simpsons 7 temporada"
                         subtitle="7 temporada"
                         link="/simpsons-7-temp"
-                        img=""
+                        img="https://media.fstatic.com/zAUwWJpqk-9YvQyK9hnBuj3Du-g=/fit-in/290x478/smart/media/movies/covers/2010/03/136bffe7b5f741f68454982f101e2287.jpg"
                     ></LayoutSimpson>
                 </div>
                 <div className="col">
@@ -66,7 +66,7 @@ export default function Simpson() {
                         title="Simpsons 8 temporada"
                         subtitle="8 temporada"
                         link="/simpsons-8-temp"
-                        img=""
+                        img="https://upload.wikimedia.org/wikipedia/en/thumb/2/2d/The_Simpsons_-_The_Complete_8th_Season.jpg/220px-The_Simpsons_-_The_Complete_8th_Season.jpg"
                     ></LayoutSimpson>
                 </div>
                 <div className="col">
@@ -74,7 +74,7 @@ export default function Simpson() {
                         title="Simpsons 9 temporada"
                         subtitle="9 temporada"
                         link="/simpsons-9-temp"
-                        img=""
+                        img="https://upload.wikimedia.org/wikipedia/en/5/5f/The_Simpsons_-_The_Complete_9th_Season.jpg"
                     ></LayoutSimpson>
                 </div>
                 <div className="col">
@@ -82,7 +82,7 @@ export default function Simpson() {
                         title="Simpsons 10 temporada"
                         subtitle="10 temporada"
                         link="/simpsons-10-temp"
-                        img=""
+                        img="https://upload.wikimedia.org/wikipedia/en/f/f0/The_Simpsons_-_The_Complete_10th_Season.jpg"
                     ></LayoutSimpson>
                 </div>
                 <div className="col">
@@ -90,7 +90,7 @@ export default function Simpson() {
                         title="Simpsons 11 temporada"
                         subtitle="11 temporada"
                         link="/simpsons-11-temp"
-                        img=""
+                        img="https://upload.wikimedia.org/wikipedia/en/e/e0/The_Simpsons_-_The_11th_Season.jpg"
                     ></LayoutSimpson>
                 </div>
                 <div className="col">
@@ -98,7 +98,7 @@ export default function Simpson() {
                         title="Simpsons 12 temporada"
                         subtitle="12 temporada"
                         link="/simpsons-12-temp"
-                        img=""
+                        img="https://upload.wikimedia.org/wikipedia/en/1/1d/The_Simpsons_-_The_12th_Season.jpg"
                     ></LayoutSimpson>
                 </div>
                 <div className="col">
@@ -106,7 +106,7 @@ export default function Simpson() {
                         title="Simpsons 13 temporada"
                         subtitle="13 temporada"
                         link="/simpsons-13-temp"
-                        img=""
+                        img="https://upload.wikimedia.org/wikipedia/en/f/fc/The_Simpsons_-_The_13th_Season.jpg"
                     ></LayoutSimpson>
                 </div>
                 <div className="col">
@@ -114,7 +114,7 @@ export default function Simpson() {
                         title="Simpsons 14 temporada"
                         subtitle="14 temporada"
                         link="/simpsons-14-temp"
-                        img=""
+                        img="https://upload.wikimedia.org/wikipedia/en/9/9c/The_Simpsons_-_The_14th_Season.jpg"
                     ></LayoutSimpson>
                 </div>
                 <div className="col">
@@ -122,7 +122,7 @@ export default function Simpson() {
                         title="Simpsons 15 temporada"
                         subtitle="15 temporada"
                         link="/simpsons-15-temp"
-                        img=""
+                        img="https://upload.wikimedia.org/wikipedia/en/e/ed/The_Simpsons_-_The_15th_Season.jpg"
                     ></LayoutSimpson>
                 </div>
                 <div className="col">
@@ -130,7 +130,7 @@ export default function Simpson() {
                         title="Simpsons 16 temporada"
                         subtitle="16 temporada"
                         link="/simpsons-16-temp"
-                        img=""
+                        img="https://upload.wikimedia.org/wikipedia/en/0/0a/The_Simpsons_-_The_16th_Season.jpg"
                     ></LayoutSimpson>
                 </div>
                 <div className="col">
@@ -138,7 +138,7 @@ export default function Simpson() {
                         title="Simpsons 17 temporada"
                         subtitle="17 temporada"
                         link="/simpsons-17-temp"
-                        img=""
+                        img="https://upload.wikimedia.org/wikipedia/en/c/cf/The_Simpsons_-_The_17th_Season.jpg"
                     ></LayoutSimpson>
                 </div>
                 <div className="col">
@@ -146,7 +146,7 @@ export default function Simpson() {
                         title="Simpsons 18 temporada"
                         subtitle="18 temporada"
                         link="/simpsons-18-temp"
-                        img=""
+                        img="https://upload.wikimedia.org/wikipedia/en/5/51/The_Simpsons_season_18.png"
                     ></LayoutSimpson>
                 </div>
                 <div className="col">
@@ -154,7 +154,7 @@ export default function Simpson() {
                         title="Simpsons 19 temporada"
                         subtitle="19 temporada"
                         link="/simpsons-19-temp"
-                        img=""
+                        img="https://upload.wikimedia.org/wikipedia/en/8/8e/Thesimpsonsseason19dvdcover.jpg"
                     ></LayoutSimpson>
                 </div>
                 <div className="col">
@@ -162,7 +162,7 @@ export default function Simpson() {
                         title="Simpsons 20 temporada"
                         subtitle="20 temporada"
                         link="/simpsons-20-temp"
-                        img=""
+                        img="https://upload.wikimedia.org/wikipedia/en/e/e8/The_Simpsons_-_The_Complete_20th_Season.jpg"
                     ></LayoutSimpson>
                 </div>
                 <div className="col">
@@ -170,7 +170,7 @@ export default function Simpson() {
                         title="Simpsons 21 temporada"
                         subtitle="21 temporada"
                         link="/simpsons-21-temp"
-                        img=""
+                        img="https://1.bp.blogspot.com/-UeNOUNHzlSk/XdvZ25eSAAI/AAAAAAAAAfA/fv85YvWeIYkU-qC1nyR0FI9OxZBacrILQCLcBGAsYHQ/s1600/zw4BQOMFlM8cu1beAUJhY6Rk76T.jpg"
                     ></LayoutSimpson>
                 </div>
                 <div className="col">
@@ -178,7 +178,7 @@ export default function Simpson() {
                         title="Simpsons 22 temporada"
                         subtitle="22 temporada"
                         link="/simpsons-22-temp"
-                        img=""
+                        img="https://1.bp.blogspot.com/-xQ0to20MUPE/XdvfLoWeOqI/AAAAAAAAAfc/mVi5b5TdDUgW4pEf-pmRmC9Fi6JtBI2EQCLcBGAsYHQ/s1600/fOX0HS4Tn8w21hwhOa8YrAakv7k.jpg"
                     ></LayoutSimpson>
                 </div>
                 <div className="col">
@@ -186,7 +186,7 @@ export default function Simpson() {
                         title="Simpsons 23 temporada"
                         subtitle="23 temporada"
                         link="/simpsons-23-temp"
-                        img=""
+                        img="https://1.bp.blogspot.com/-prwwGrUnJIo/Xd1Qly0rqMI/AAAAAAAAAiE/MmGouh4o1pcJq8se7y4HK3MpEO980MsQACLcBGAsYHQ/s1600/6SKdh36MCY9fzjen19mIBiZI8p6.jpg"
                     ></LayoutSimpson>
                 </div>
                 <div className="col">
@@ -194,7 +194,7 @@ export default function Simpson() {
                         title="Simpsons 24 temporada"
                         subtitle="24 temporada"
                         link="/simpsons-24-temp"
-                        img=""
+                        img="https://1.bp.blogspot.com/-TBK52UyGKsA/Xdv_pKboNgI/AAAAAAAAAf4/i3LsfO6vszoVhAYmDHY-vgmx8Xt3RYRMgCLcBGAsYHQ/s1600/lossimpsonstemporada24.png"
                     ></LayoutSimpson>
                 </div>
                 <div className="col">
@@ -202,7 +202,7 @@ export default function Simpson() {
                         title="Simpsons 25 temporada"
                         subtitle="25 temporada"
                         link="/simpsons-25-temp"
-                        img=""
+                        img="https://upload.wikimedia.org/wikipedia/pt/8/87/Simpsons-25.jpg"
                     ></LayoutSimpson>
                 </div>
                 <div className="col">
@@ -210,7 +210,7 @@ export default function Simpson() {
                         title="Simpsons 26 temporada"
                         subtitle="26 temporada"
                         link="/simpsons-26-temp"
-                        img=""
+                        img="https://upload.wikimedia.org/wikipedia/pt/8/8a/Simpsons-26.png"
                     ></LayoutSimpson>
                 </div>
                 <div className="col">
@@ -218,7 +218,7 @@ export default function Simpson() {
                         title="Simpsons 27 temporada"
                         subtitle="27 temporada"
                         link="/simpsons-27-temp"
-                        img=""
+                        img="https://1.bp.blogspot.com/-HC7LeH_aqkg/XdwVSC5eTUI/AAAAAAAAAgo/-3kq_tOxxTw-pqtZE2mDFmnj9bUgR4-VwCLcBGAsYHQ/s1600/The.Simpsons.Poster27-1.jpg"
                     ></LayoutSimpson>
                 </div>
                 <div className="col">
@@ -226,7 +226,7 @@ export default function Simpson() {
                         title="Simpsons 28 temporada"
                         subtitle="28 temporada"
                         link="/simpsons-28-temp"
-                        img=""
+                        img="https://upload.wikimedia.org/wikipedia/pt/9/90/The_Simpsons_28.png"
                     ></LayoutSimpson>
                 </div>
                 <div className="col">
@@ -234,7 +234,7 @@ export default function Simpson() {
                         title="Simpsons 29 temporada"
                         subtitle="29 temporada"
                         link="/simpsons-29-temp"
-                        img=""
+                        img="https://media.fstatic.com/RVFTVGY0O1yFk5oCravzkpM3myI=/290x478/smart/media/movies/covers/2019/06/Os_Simpstons_29_temproada_torrent_download_dublada_completa_hd.jpg"
                     ></LayoutSimpson>
                 </div>
                 <div className="col">
@@ -242,7 +242,7 @@ export default function Simpson() {
                         title="Simpsons 30 temporada"
                         subtitle="30 temporada"
                         link="/simpsons-30-temp"
-                        img=""
+                        img="https://media.fstatic.com/vFwHCqfAWZyfp5GjtVtxYtUBeAs=/210x312/smart/media/movies/covers/2019/06/l18xQsO27KFJwsj35JaAsG95dN7.jpg"
                     ></LayoutSimpson>
                 </div>
                 <div className="col">
@@ -250,7 +250,7 @@ export default function Simpson() {
                         title="Simpsons 31 temporada"
                         subtitle="31 temporada"
                         link="/simpsons-31-temp"
-                        img=""
+                        img="https://media.fstatic.com/lKCjH5bY9N2lqLxX3JzhEnw_wFk=/290x478/smart/media/movies/covers/2019/08/2019-08-07_1.png"
                     ></LayoutSimpson>
                 </div>
                 <div className="col">
@@ -258,7 +258,7 @@ export default function Simpson() {
                         title="Simpsons 32 temporada"
                         subtitle="32 temporada"
                         link="/simpsons-32-temp"
-                        img=""
+                        img="https://1.bp.blogspot.com/-uGbVHFb3jOI/X9EgcRw92hI/AAAAAAAAFho/nDyYRmVqs9ItfgPSqIW3qGSLHSW9k9dygCLcBGAsYHQ/s1280/maxresdefault.jpg"
                     ></LayoutSimpson>
                 </div>
                 
