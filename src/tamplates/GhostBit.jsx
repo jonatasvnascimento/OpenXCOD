ba61a96d790df2f3f24253bedce533420ef249c2
jonatasvnascimento <jonatasv.nascimento@gmail.com>
2021-06-20 08:49:08 -0300

    rename

--- src/tamplates/GhostBit.jsx
+++ src/tamplates/GhostBit.jsx
@@ -4,7 +4,7 @@ import {Link} from 'react-router-dom'
 export default function GhostBit() {
     return (
         <div className="container mt-3">
-            <Link to="https://ghostbit.com.br">GhostBit</Link>
+            <Link to="https://openkodo.vercel.app">OpenKODO site</Link>
         </div>
     )
 }
