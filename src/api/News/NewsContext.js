062613cf9839a62363afd813c158c8f43888deb9
@jonatas.viana <56093381+jonatasvnascimento@users.noreply.github.com>
2021-07-08 19:10:36 -0300

    add axios end remove fetch

--- src/api/News/NewsContext.js
+++ src/api/News/NewsContext.js
@@ -1,22 +1,33 @@
 import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 export default function NewsContext() {
 
     const [data, setData] = useState([])
-    useEffect(() => {
-        async function getContent() {
-            try {
-                const response = await fetch('http://localhost:3001/')
-                const data = await response.json()
-                setData(data)
-                // console.log(data)
-            } catch (error) {
-                console.error(error)
-            }
-        }
-        getContent()
 
+    useEffect(() => {
+        axios.get('http://localhost:3001/')
+            .then(res => {
+                setData(res.data)
+                // console.log(res)
+            })
     }, [data])
+
+    //Codigo de chamada api usando fetch
+    // useEffect(() => {
+    //     async function getContent() {
+    //         try {
+    //             const response = await fetch('http://localhost:3001/')
+    //             const data = await response.json()
+    //             setData(data)
+    //             // console.log(data)
+    //         } catch (error) {
+    //             console.error(error)
+    //         }
+    //     }
+    //     getContent()
+
+    // }, [data])
 
     return (
         <div className="container mt-3">
