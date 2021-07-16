7ac6fc399d016a2d86ff68227df673b602159b1b
@jonatas.viana <jonatasv.nascimento@gmail.com>
2021-07-16 18:21:53 +0000

    fix + or - toggle

--- src/tamplates/Header.jsx
+++ src/tamplates/Header.jsx
@@ -22,18 +22,19 @@ const Example = (props) => {
             <div className="container d-flex align-items-center justify-content-between">
                 <h1 className="logo"><a href="index.html">OpenXCOD></a></h1>
 
-                <nav id="navbar" className="navbar">
-                        <ul>
-                            <li><Link to="/" className="nav-link scrollto">Home</Link></li>
 
-                            <li><Link to="/noticias" className="nav-link scrollto" href="#about">Noticias</Link></li>
-                            <li><Link to="/tools" className="nav-link scrollto" href="#services">Ferramentas</Link></li>
-                            <li><Link to="/" className="nav-link scrollto " href="#portfolio">Portfolio</Link></li>
-                            <li><Link to="/" className="nav-link scrollto" href="#team">Entretenimento</Link></li>
-                            <li className="dropdown"><Link to="/" href="#"><span>API</span> <i className="bi bi-chevron-down"></i></Link>
-                                <ul>
-                                    <li><Link to="/news" href="#">News Teste</Link></li>
-                                    {/* <li className="dropdown"><Link to="/" href="#"><span>Deep Drop Down</span> <i className="bi bi-chevron-right"></i></Link>
+                <nav id="navbar" className="navbar">
+                    <ul>
+                        <li><Link to="/" className="nav-link scrollto">Home</Link></li>
+
+                        <li><Link to="/noticias" className="nav-link scrollto" href="#about">Noticias</Link></li>
+                        <li><Link to="/tools" className="nav-link scrollto" href="#services">Ferramentas</Link></li>
+                        <li><Link to="/" className="nav-link scrollto " href="#portfolio">Portfolio</Link></li>
+                        <li><Link to="/" className="nav-link scrollto" href="#team">Entretenimento</Link></li>
+                        <li className="dropdown"><Link to="/" href="#"><span>API</span> <i className="bi bi-chevron-down"></i></Link>
+                            <ul>
+                                <li><Link to="/news" href="#">News Teste</Link></li>
+                                {/* <li className="dropdown"><Link to="/" href="#"><span>Deep Drop Down</span> <i className="bi bi-chevron-right"></i></Link>
                                         <ul>
                                             <li><Link to="/" href="#">Deep Drop Down 1</Link></li>
                                             <li><Link to="/" href="#">Deep Drop Down 2</Link></li>
@@ -45,23 +46,24 @@ const Example = (props) => {
                                     <li><Link to="/" href="#">Drop Down 2</Link></li>
                                     <li><Link to="/" href="#">Drop Down 3</Link></li>
                                     <li><Link to="/" href="#">Drop Down 4</Link></li> */}
-                                </ul>
-                            </li>
-                            {/* <li><Link to="/" className="nav-link scrollto" href="#contact">Contact</Link></li> */}
-                        </ul>
+                            </ul>
+                        </li>
+                        {/* <li><Link to="/" className="nav-link scrollto" href="#contact">Contact</Link></li> */}
+                    </ul>
 
-                    </nav>
-                
+
+                </nav>
+
                 <NavbarToggler onClick={toggle} >
-                    x
+                    <i className="bi bi-list mobile-nav-toggle"></i>
                 </NavbarToggler>
                 <Collapse isOpen={isOpen} navbar >
                     {/* <NavItem>
                         <Link to="/entretenimento" className="nav-link">Entretenimento</Link>
                     </NavItem> */}
-                        <nav id="navbar" className="navbar">
-                               <Link to="/" className="nav-link scrollto">Home</Link>
-                        </nav>
+                    <nav id="navbar" className="navbar">
+                        <Link to="/" className="nav-link scrollto">Home</Link>
+                    </nav>
 
 
                     {/* <UncontrolledDropdown nav inNavbar>
